638e52906fc2c6a8aecdf94a5676dd00dd3424c8
Samuel Herts <sdherts@gmail.com>
2021-12-03 13:13:39 -0600

    Update SHA256.py

--- SHA/SHA256.py
+++ SHA/SHA256.py
@@ -1,4 +1,4 @@
-import hashlib
+from hashlib import sha256 # For verifying the accuracy.
 
 # Initial hash values to help seed the output.
 
@@ -15,76 +15,57 @@ K_constants = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f
 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 
-debug = True
-
-
 def SHA256(message): 
     # Implementation based on NIST.FIPS.180-4
-
-    if (debug): print("Starting SHA256 Function")
 
     # Get the initial values
     K = K_constants
     H = H_constants
 
     # Pad the message to be multiples of 512 bits
-    padded_message = Preprocess(message)
-    if (debug): print(f'{padded_message=}')
-    if (debug): print(f'{len(padded_message)=}')
+    padded_message = Preprocess(message)    
 
     # Get the number of cycles needed
-    N = len(padded_message) // 512
-    if (debug): print(f'{N=}')    
+    N = len(padded_message) // 512     
 
-    for i in range(1, N+1):
-        if (debug): print(f'{i=}')
+    for i in range(1, N+1):        
         W = []
 
         # Prep only the portion of the message we are working on, in blocks of 512
-        W = Prep_Message_Schedule(padded_message[512*(i-1):512*i], W)
-        if (debug): print(f'W={[hex(x) for x in W]}')
+        start = 512 * (i-1)
+        end = 512 * i       
+        W = Prep_Message_Schedule(padded_message[start : end], W)
+                
+        # Set the eight working variables by unpacking            
+        a, b, c, d, e, f, g, h = H
         
-        # Set the eight working variables
-        if (debug): print(f'H={[hex(x) for x in H]}')    
-        a = H[0]
-        b = H[1]
-        c = H[2]
-        d = H[3]
-        e = H[4]
-        f = H[5]
-        g = H[6]
-        h = H[7]
-
-        
-        for t in range(63):            
-
-            T_one = (h + Sigma_Upper(e, 6, 11, 25) + Choose(e, f, g) + K[t] + W[t] )% (2 ** 32) 
-            T_two = Sigma_Upper(a, 2, 13, 22) + Majority(a, b, c)  
+        for t in range(64):
+            # Cycle through the 64 base values, and move and shift and XOR and combine everything                     
+            T_one = (h + Sigma_Upper(e, 6, 11, 25) + Choose(e, f, g) + K[t] + W[t] ) % (2 ** 32)
+            
+            T_two = (Sigma_Upper(a, 2, 13, 22) + Majority(a, b, c)) % (2 ** 32)
+            
             h = g
             g = f
             f = e
-            e = d + T_one
+            e = (d + T_one) % (2 ** 32)
             d = c
             c = b
             b = a
-            a = T_one + T_two            
-            
-            if (debug): print(f'{t=},a={hex(a)},b={hex(b)},c={hex(c)},d={hex(d)},e={hex(e)},f={hex(f)},g={hex(g)},h={hex(h)}')
+            a = (T_one + T_two) % (2 ** 32)             
 
         # Compute the ith intermediate hash value 
-        H[0] = (a + H[0]) % (2 ** 32)
-        H[1] = (b + H[1]) % (2 ** 32)
-        H[2] = (c + H[2]) % (2 ** 32)
-        H[3] = (d + H[3]) % (2 ** 32)
-        H[4] = (e + H[4]) % (2 ** 32)
-        H[5] = (f + H[5]) % (2 ** 32)
-        H[6] = (g + H[6]) % (2 ** 32)
-        H[7] = (h + H[7]) % (2 ** 32)
-
-        if (debug): print(f'H={[hex(x) for x in H]}')
+        H[0] = (a + H[0]) 
+        H[1] = (b + H[1]) 
+        H[2] = (c + H[2]) 
+        H[3] = (d + H[3]) 
+        H[4] = (e + H[4]) 
+        H[5] = (f + H[5]) 
+        H[6] = (g + H[6]) 
+        H[7] = (h + H[7])        
     
     # Append the H values to form the final result.
-    Final_Message = [format(x, '08x') for x in H]
+    Final_Message = [format(x % (2 ** 32), '08x') for x in H]
     return ''.join(Final_Message)
                 
 
@@ -93,12 +74,18 @@ def Left_Rotate(input, amount, bitSize=32):
     # For Example: 11110000, 1 -> 11100001
     # For Example: 10101010, 1 -> 01010101
     # For Example: 11110000, 3 -> 10000111
-    return (input << amount & (2**bitSize - 1)) | (input >> (bitSize - amount))
+    return (input << amount%bitSize) & (2**bitSize-1) | ((input & (2**bitSize-1)) >> (bitSize-(amount%bitSize)))
 
 def Right_Rotate(input, amount, bitSize=32):
-    return (input >> amount & (2**bitSize - 1)) | (input << (bitSize - amount))
+    # Shift the input bits right by amount circularly and ensure no padding bits are added, bitwise OR that value with the input shifted to the left by the number of total bits
+    # For Example: 11110000, 1 -> 01111000
+    # For Example: 10101010, 1 -> 01010101
+    # For Example: 11110000, 3 -> 00011110
+    return ((input & (2**bitSize-1)) >> amount%bitSize) | (input << (bitSize-(amount%bitSize)) & (2**bitSize-1))
+ 
 
 def Right_Shift(input, amount, bitSize=32):
+    # Shift the input bits right by amount and pad the left with 0's
     return (input >> amount) & (2**bitSize - 1)
 
 def Choose(x, y, z):
@@ -121,52 +108,38 @@ def Majority(x, y, z):
     return (x & y) ^ (x & z) ^ (y & z)
 
 def Sigma_Upper(x, first, second, third):
+    # XOR of right rotated input to jumble it
     return Right_Rotate(x, amount=first, bitSize=32) ^ Right_Rotate(x, amount=second, bitSize=32) ^ Right_Rotate(x, amount=third, bitSize=32)
 
-def Signma_Lower(x, first, second, third):
-        return Right_Rotate(x, amount=first, bitSize=32) ^ Right_Rotate(x, amount=second, bitSize=32) ^ Right_Shift(x, amount=third, bitSize=32)
+def Sigma_Lower(x, first, second, third):
+    # XOR of right rotated and right shifted input to jumble it
+    return Right_Rotate(x, amount=first, bitSize=32) ^ Right_Rotate(x, amount=second, bitSize=32) ^ Right_Shift(x, amount=third, bitSize=32)
 
 
 def Get_Bit_Count(message:str):
     # Gets the total number of bits in a string assuming 8 bits per byte, with one byte per letter
-    if (debug): print("Starting Get_Bit_Count Function")
     return len(message.encode('ascii')) * 8
 
 def get_binary_list(message:str)-> list:
-    if (debug): print("Starting get_binary_list Function")
     # convert the string to ascii bytes
     m_bytes = message.encode('ascii')
     # format the bytes into bits
     return [format(x, '08b') for x in m_bytes]
 
 def Preprocess(message):
-    if (debug): print("Starting Preprocess Function")
     # First pad the message to a length of a multiple of 512 bits
     bit_count = Get_Bit_Count(message)
-    if (debug): print(f'{bit_count=}')
-    
+        
     padding = (447 - bit_count) % 512
-    if (debug): print(f'{padding=}')
-    
-
-    bit_message = get_binary_list(message)
-    if (debug): print(f'{bit_message=}')
-
-    # Append a 1, then fill the rest with 0's until mod 512 by appending blocks of 8 zeros.
-    
-    if (debug): print("Appending 1")
-    bit_message.append('1')
-
-    if (debug): print(f"Appending {'0'*padding}")       
-    bit_message.append('0'*padding)
-
+        
+    bit_message = get_binary_list(message)    
+    # Append a 1, then fill the rest with 0's until mod 512 by appending blocks of 8 zeros.          
+    bit_message.append('1' + '0' * padding)
     # Finally append the length of the message in binary
-    bit_message.append(format(bit_count, '064b'))
-    
+    bit_message.append(format(bit_count, '064b'))    
     return ''.join(bit_message)
-
+    
 def Prep_Message_Schedule(input, schedule):
-    if (debug): print("Starting Prep_Message_Schedule Function")
     for t in range(80):
         if t <= 15:
             # M_sub-t_exp-i
@@ -178,29 +151,26 @@ def Prep_Message_Schedule(input, schedule):
             schedule.extend([ int(sub_block, 2) ])
         else:
             # Add a jumbled version of the message onto the message schedule
-            sig_1_256 = Signma_Lower(schedule[t-2], 17, 19, 10)
-            sig_0_256 = Signma_Lower(schedule[t-15], 7, 18, 3)
-            chunk = sig_1_256 + schedule[t - 7] +  sig_0_256 +  schedule[t-16]
+            sig_1_256 = Sigma_Lower(schedule[t-2], 17, 19, 10)
+            sig_0_256 = Sigma_Lower(schedule[t-15], 7, 18, 3)
+            chunk = ( sig_1_256 + schedule[t - 7] +  sig_0_256 +  schedule[t-16] ) % (2 ** 32)
             schedule.append(chunk)
-
     return schedule
 
 
 def Main():
-    myString = 'abc'
-    #myString = 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'
-    #if (debug): 
-    print(f'{len(myString)=}')
+    #myString = 'abc'
+    myString = 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'    
     
     print(f'String to encode: {myString}\n\nImplementation:\n')    
-    mySHA1 = SHA256(myString)
-    print(mySHA1)
+    mySHA256 = SHA256(myString)
+    print(mySHA256)
     print('\nLibrary Implementation:\n')
-    libSHA1 = hashlib.sha256()
+    libSHA1 = sha256()
     libSHA1.update(myString.encode("ASCII"))
     print(libSHA1.hexdigest())
     
-    if (mySHA1 == libSHA1.hexdigest()):
+    if (mySHA256 == libSHA1.hexdigest()):
         print("They match!")
     else:
         print("No match, something went wrong!")
